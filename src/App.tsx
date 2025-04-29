416e64fb4e149e0c07ffbe32461df8a13a4fe199
Felicia Chen <feliciachen18@gmail.com>
2025-04-28 18:49:10 -0700

    map in the background

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
+"use client"
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { APIProvider, Map, useMapsLibrary, useMap } from '@vis.gl/react-google-maps';
 
 function App() {
+  const position = { lat: 43.6532, lng: -79.3832 }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{height: "100vh", width: "100vw"}}>
+      <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string}>
+        <Map center={position} zoom={9}></Map>
+      </APIProvider>
     </div>
   );
 }
